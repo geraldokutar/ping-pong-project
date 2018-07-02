c39f659c6ae93ee0d184e34ff6f30d3ea6b4376a
Gerald Okuta <geraldpilk@gmail.com>
2018-07-02 07:30:13 +0300

    ping pong

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,35 @@
-function submitInfo(){
-  var number1 = document.getElemntById("number").value;
+//Business Logic
+var countUp = function(countTo){
+ var result = [];
+ for(var i = 1; i <= countTo; i++){
+     var index = result.indexOf(i);
+     if(i % 15 === 0) {
+       result.splice(index, 0);
+       result.push('pingpong');
+     }else if(i % 5 === 0) {
+       result.splice(index, 0);
+       result.push('pong');
+     }else if(i % 3 === 0) {
+       result.splice(index, 0);
+       result.push('ping');
+     }else{
+       result.push(i);
+     }
+ }
 
-  if(number == "1"){
-    confirm("you got it");
-  })
-})
+ return result;
+};
+
+//User Interface Logic
+$(document).ready(function(){
+ $("#userInput").submit(function(event){
+   $("#output").empty();
+   var countTo = parseInt($("input#countTo").val());
+   var output = countUp(countTo);
+
+   output.forEach(function(item){
+     $("#output").append('<li>' + item + '</li>');
+   });
+   event.preventDefault();
+ });
+});
